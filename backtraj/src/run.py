b0ee4442eaa9116c0bc99129a1595ace855cb04d
换季梵高 <150894264+RuleEarth336123@users.noreply.github.com>
2024-09-09 18:03:34 +0800

    Delete backtraj directory

--- backtraj/src/run.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-# @Time        : 2024/04/22
-# @Author      : yclu
-# @Email       : yclu@aioaiofm.ac.cn
-# @File        : draw.py
-# @Project     : NovaPrograms
-# @Description : 聚类后向轨迹
